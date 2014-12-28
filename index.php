ea3799641fd0b11af4ec87fc68e8a95179d775f9
giuseppenantista <giuseppe.nantista@gmail.com>
2014-12-28 22:40:23 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>IC Radius Management :: Benvenuto</title>
+<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+
+<table width="450" cellpadding="5" cellspacing="5" border="1">
+
+<!-- TABELLA INTERNA CONTENENTE IL FORM -->
+
+ <form action="search.php">
+ <td style="border-bottom:1px solid gray; border-right: 1px solid gray;"> <p style="padding-left:5px;">
+ <p style="padding-left:10px;">
+              <input  type="text" name="ricerca">
+                        <select size="1" name="tipo">
+                        <option>username</option>
+                        <option>ip</option>
+                        </select>
+              <input  type="submit" name="submit" value="Search">
+ </p>
+
+<!-- </form> -->
+
+</table>
+<br>
+<h3><a href='create.php'> Crea un nuovo utente </a></h3>
+<h3><a href='ippool.php'> Gestisci pool IP </a></h3>
+<h3><a href='profili.php'> Gestisci profili </a></h3>
+
+<p>
+
+</body>
+</html>
+
