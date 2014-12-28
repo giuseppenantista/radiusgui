167bc107aca2d18f6c5fc16fd8b23ef655d139ef
giuseppenantista <giuseppe.nantista@gmail.com>
2014-12-28 22:48:09 +0100

    Create blocca.php

---
+++ blocca.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>IC Radius Management :: Benvenuto</title>
+<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+<img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1>
+
+<table width="450" cellpadding="5" cellspacing="5" border="1">
+
+<?php
+        include 'db.php';
+
+        // La pagina search.php mi ha passato UserName
+
+        $UserName=$_REQUEST['UserName'];
+
+        $conn = mysql_connect($db_data_host,$db_data_user,$db_data_password);
+        if (!$conn) {
+                die('Could not connect to RADIUS DB: ' . mysql_error());
+        }
+        mysql_select_db($db_data_dbname);
+
+        $dd=mysql_query("SELECT address from ip where pool='BLOCCATI' and used=0");
+        $d=mysql_fetch_row($dd);
+        if (!$d) {
+                die('Errore, profilo non esistente: ' . mysql_error());
+        }
+        mysql_query("UPDATE radreply set Value = '$d[0]' where UserName='$UserName' and Attribute='Framed-IP-Address'");
+        mysql_query("UPDATE ip set used=1 where address='$d[0]'");
+        mysql_query("UPDATE userinfo set attivato='0' where UserName='$UserName'");
+
+        echo "<h2>UPDATE COMPLETATO <br>";
+        echo "<a href='visualizza.php?UserName=$UserName'> TORNA A SCHEDA UTENTE </a></h2>";
+
+?>
+
+</table> <p>
+
+</body>
+</html>
+
