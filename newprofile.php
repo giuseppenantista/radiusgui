1d0c832fee7a492292dbdc66c2da47c84431763e
giuseppenantista <giuseppe.nantista@gmail.com>
2014-12-29 12:10:42 +0100

    Create newprofile.php

---
+++ newprofile.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>IC Radius Management :: Risultati Ricerca</title>
+<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+
+<table border="1" cellpadding="1" cellspacing="1" style="width: 500px;">
+
+        <tbody>
+        <form action="commitnewprofile.php">
+        <p style="font-family:Century gothic, Verdana, Arial; font-size:12px; color:#0000A4; padding-left:0px">
+                <input type="text" name="profilename" size="30" value=""> Nome del profilo<br />
+                <input type="text" name="down"        size="30" value=""> Download (kb)<br />
+                <input type="text" name="up"          size="30" value=""> Upload (kb)<br />
+                <input type="text" name="priority"    size="30" value=""> Priority (1-8)<br />
+                <br>
+                <input type="submit" name="submit" value="Salva">
+        </p>
+        </form>
+
+        </tbody>
+
+</table>
+
+</body>
+</html>
