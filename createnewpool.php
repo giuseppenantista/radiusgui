f0fb1d0ef04420a4555a6f5d4d24ef2101d85aaa
giuseppenantista <giuseppe.nantista@gmail.com>
2014-12-29 12:04:39 +0100

    Create createnewpool.php

---
+++ createnewpool.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<title>IC Radius Management :: Benvenuto</title>
+<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+
+<table width="450" cellpadding="5" cellspacing="5" border="1">
+
+<?php
+        include 'db.php';
+
+        // La pagina newpool.php mi ha passato i campi poolname, net e mask
+
+        $poolname=$_REQUEST['poolname'];
+        $net=$_REQUEST['net'];
+        $mask=$_REQUEST['mask'];
+
+        $conn = mysql_connect($db_data_host,$db_data_user,$db_data_password);
+        if (!$conn) {
+                die('Could not connect to RADIUS DB: ' . mysql_error());
+        }
+        mysql_select_db($db_data_dbname);
+
+        // Qua devo mettere le query per creare gli IP
+
+        if ($mask!='255.255.255.0') {
+                echo "Netmask non supportata";
+        }
+        if ($mask=='255.255.255.0') {
+                // Normalizzo l'ip a un IP di network /24
+                $newnet = long2ip(ip2long($net) & 0xFFFFFF00);
+                for ($i=1;$i<255;++$i) {
+                        $indirizzo=long2ip(ip2long($newnet)+$i);
+                        // indirizzo contiene una stringa con l'IP
+                        mysql_query("INSERT into ip values ('$indirizzo','$poolname',0)");
+                }
+        }
+?>
+        <h2>Creazione completata <br>
+        <a href='ippool.php'> Torna a Gestione pool </a></h2>
+
+
+</table> <p>
+
+</body>
+</html>
