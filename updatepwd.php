f02b603901c249232c33afb9a4a791d5c9506086
giuseppenantista <giuseppe.nantista@gmail.com>
2014-12-29 12:16:16 +0100

    Create updatepwd.php

---
+++ updatepwd.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>IC Radius Management :: Benvenuto</title>
+<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+
+<table width="450" cellpadding="5" cellspacing="5" border="1">
+
+<?php
+        include 'db.php';
+
+        // La pagina changepwd.php mi ha passato i campi Username e password
+
+        $UserName=$_REQUEST['UserName'];
+        $password=$_REQUEST['password'];
+
+        $conn = mysql_connect($db_data_host,$db_data_user,$db_data_password);
+        if (!$conn) {
+                die('Could not connect to RADIUS DB: ' . mysql_error());
+        }
+        mysql_select_db($db_data_dbname);
+
+        mysql_query("UPDATE userinfo set password='$password' where Username='$UserName'");
+        mysql_query("UPDATE radcheck set Value='$password' where Username='$UserName' and Attribute='Password'");
+
+        echo "<h2>UPDATE COMPLETATO <br>";
+        echo "<a href='visualizza.php?UserName=$UserName'> TORNA A SCHEDA UTENTE </a></h2>";
+
+?>
+
+</table> <p>
+
+</body>
+</html>
