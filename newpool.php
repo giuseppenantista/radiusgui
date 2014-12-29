3804db8bc9221a5ec895c76fb8788351e37c18d6
giuseppenantista <giuseppe.nantista@gmail.com>
2014-12-29 12:09:55 +0100

    Create newpool.php

---
+++ newpool.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>IC Radius Management :: Risultati Ricerca</title>
+<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+
+<table border="1" cellpadding="1" cellspacing="1" style="width: 500px;">
+
+        <tbody>
+
+        <form action="createnewpool.php">
+        <p style="font-family:Century gothic, Verdana, Arial; font-size:12px; color:#0000A4; padding-left:0px">
+                <input type="text" name="poolname"      size="30" value=""> Nome del Pool<br />
+                <input type="text" name="net"           size="30" value=""> Network address<br />
+                <input type="text" name="mask"          size="30" value="255.255.255.0"> Netmask<br />
+                <br>
+                <input type="submit" name="submit" value="Crea">
+        </p>
+        </form>
+
+        </tbody>
+
+
+</table>
+
+</body>
+</html>
