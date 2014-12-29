2fb31f52e6b6cd913a7fc14f9d77131e5d23b56e
giuseppenantista <giuseppe.nantista@gmail.com>
2014-12-29 12:12:18 +0100

    Create saveprofile.php

---
+++ saveprofile.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>IC Radius Management :: Benvenuto</title>
+<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+
+<table width="450" cellpadding="5" cellspacing="5" border="1">
+
+<?php
+        include 'db.php';
+
+        // La pagina editprofile.php mi ha passato i campi profilename,down,up,priority
+
+        $profilename=$_REQUEST['profilename'];
+        $down=$_REQUEST['down'];
+        $up=$_REQUEST['up'];
+        $priority=$_REQUEST['priority'];
+
+        $conn = mysql_connect($db_data_host,$db_data_user,$db_data_password);
+        if (!$conn) {
+                die('Could not connect to RADIUS DB: ' . mysql_error());
+        }
+        mysql_select_db($db_data_dbname);
+
+        mysql_query("UPDATE profiles set down='$down',up='$up',priority='$priority' where profilename='$profilename'");
+        $stringa=$up.'k/'.$down.'k '.$up.'k/'.$down.'k '.$up.'k/'.$down.'k '.'1/1 '.$priority.' '.$up.'k/'.$up.'k';
+//      echo "$stringa";
+        mysql_query("UPDATE profiles set rate='$stringa' where profilename='$profilename'");
+
+        echo "<h2>UPDATE COMPLETATO <br>";
+        echo "<a href='profili.php'> TORNA A ELENCO PROFILI </a></h2>";
+
+?>
+
+</table> <p>
+
+</body>
+</html>
+
