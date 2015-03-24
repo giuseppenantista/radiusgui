505934bbbe04534e02588e803806fe2ea3003c81
giuseppenantista <giuseppe.nantista@gmail.com>
2015-03-24 22:42:52 +0100

    Update commitnewprofile.php

--- commitnewprofile.php
+++ commitnewprofile.php
@@ -1,13 +1,13 @@
 <html>
 <head>
-<title>IC Radius Management :: Benvenuto</title>
-<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
-<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<title>RadiusGUI :: Benvenuto</title>
+<link rel="shortcut icon" href="favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="styles.css?s=1364417489" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 </head>
 
 <body>
-<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+<img src="logo.png" id="logo" border="0" alt="RadiusGUI"><h1>RadiusGUI Interface</h1>
 
 <table width="450" cellpadding="5" cellspacing="5" border="1">
 
@@ -29,7 +29,6 @@
 
         mysql_query("INSERT into profiles (profilename,down,up,priority) values ('$profilename','$down','$up','$priority')");
         $stringa=$up.'k/'.$down.'k '.$up.'k/'.$down.'k '.$up.'k/'.$down.'k '.'1/1 '.$priority.' '.$up.'k/'.$up.'k';
-//      echo "$stringa";
         mysql_query("UPDATE profiles set rate='$stringa' where profilename='$profilename'");
 
         echo "<h2>PROFILO CREATO <br>";
