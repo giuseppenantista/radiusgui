f48792bb8c4755f73b00254332da0303253f7dbc
giuseppenantista <giuseppe.nantista@gmail.com>
2015-03-25 17:48:20 +0100

    Update newpool.php

--- newpool.php
+++ newpool.php
@@ -1,13 +1,13 @@
 <html>
 <head>
-<title>IC Radius Management :: Risultati Ricerca</title>
-<link rel="shortcut icon" href="../skins/larry/images/favicon.ico"/>
-<link rel="stylesheet" type="text/css" href="../skins/larry/styles.css?s=1364417489" />
+<title>RadiusGUI :: Benvenuto</title>
+<link rel="shortcut icon" href="favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="styles.css?s=1364417489" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 </head>
 
 <body>
-<a href="http://radius.sra.mlib.cnr.it/" target="_self"><img src="../skins/larry/images/logo.png" id="logo" border="0" alt="IC Radius Management"><h1>IC Radius Management Interface</h1></a>
+<img src="logo.png" id="logo" border="0" alt="RadiusGUI"><h1>RadiusGUI Interface</h1>
 
 <table border="1" cellpadding="1" cellspacing="1" style="width: 500px;">
 
